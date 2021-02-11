1b47ee0a2e691b3feeffb0e46103c642b19f4089
Omar Imam <omarimamdev@gmail.com>
2021-02-10 21:05:31 -0600

    Initial Commit

---
+++ omarImamC867/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  omarImamC867
+//
+//  Created by Omar Imam on 2/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
